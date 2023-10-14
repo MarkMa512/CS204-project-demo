4adddba3c1d06ac76ca916cffe7cd044d737aa7a
MarkMa512 <ningzhi04@hotmail.com>
2023-10-14 21:51:04 +0800

    Added IXP identification

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import logging
 from util.speed_test import ping_test, speed_test
 from util.trace_route import trace_route, get_locations
 from util.network import get_wifi_info_macos
-
+from caida.ip_map_ixp import parseJSONL, printIXs
 
 logging.basicConfig(format="%(asctime)s %(levelname)s %(filename)s:%(funcName)s():%(lineno)i: %(message)s",
                     datefmt="%Y-%m-%d %H:%M:%S", level=logging.INFO)
@@ -12,8 +12,6 @@ logging.basicConfig(format="%(asctime)s %(levelname)s %(filename)s:%(funcName)s(
 
 logger:logging.Logger = logging.getLogger(__name__)
     
-    
-
 if __name__ == "__main__":
 
     # Get the WiFi information
@@ -41,3 +39,9 @@ if __name__ == "__main__":
     print("List of hops identified and their locations: ")
     for ip_address, location in ip_address_location_dict.items():
         print(f"{ip_address} : {location}")
+    
+    # Analyse IXPs 
+    # with open("ixs_202307.jsonl") as ixp_jsonl: 
+    print("List of IXP found: ")
+    ixs = parseJSONL("ixs_202307.jsonl")
+    printIXs(traceroute_hops, ixs)
